a92ff6abc901dde1e0718fc05463ed6dfe0e4bea
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2020-05-01 19:45:05 +0200

    Improve development for browser compatibility
    Add browserslist, stylelint, eslint-compat

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:compat/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
@@ -32,8 +33,18 @@ module.exports = {
         config: './config/webpack.common.js'
       }
     },
+    polyfills: [
+      // Example of marking entire API and all methods and properties as polyfilled
+      "Promise",
+      // Example of marking specific method of an API as polyfilled
+      "WebAssembly.compile",
+      // Example of API with no property (i.e. a function)
+      "fetch",
+      // Example of instance method, must add `.prototype.`
+      "Array.prototype.push"
+    ]
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'compat', 'prettier'],
   rules: {
     'import/extensions': [
       'error',
@@ -49,6 +60,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.*', 'config/*'] }],
+    'compat/compat': 'warn'
   },
   overrides: [
     {
