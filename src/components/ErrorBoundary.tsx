422789deb79076ae747c6cd39bf09cb512a95bca
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2020-07-14 11:31:54 +0200

    Add error boundary.

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Box } from '@material-ui/core'
+
+type State = {
+  hasError: boolean
+}
+
+class ErrorBoundary extends React.Component<{}, State> {
+  constructor(props: {}) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: unknown) {
+    console.error('Catched error in boundary:', error, errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" flexGrow={1}>
+          <h1>Something went wrong</h1>
+          <h3>Please try again</h3>
+        </Box>
+      )
+    }
+
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
