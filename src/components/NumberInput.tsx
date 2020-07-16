7851b0e9b326eb57126c14f2676d11cbf5b27775
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2020-07-16 09:59:12 +0200

    Fix race condition in Number Input.

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -15,48 +15,61 @@ const NumberInput: React.FC<{
   outerValue: number
   onChange: (value: number) => void
   min?: number
-}> = ({ outerValue, onChange, min = 0 }) => {
+}> = ({ outerValue, min = 0, onChange: commit }) => {
   const css = useStyles()
+
   const [tempValue, setTempValue] = React.useState(`${outerValue}`) // Needs to be string to allow temporarily invalid values
 
   // Sync inner temporary value to outer value.
   // This is especially required when the outer value is changed by another component.
-  React.useEffect(() => setTempValue(`${outerValue}`), [outerValue])
-
-  const debouncedChange = React.useRef(
-    // Prepare debounced calls
-    _.debounce((newValue: number) => onChange(newValue), 300, { leading: true, trailing: true })
+  const debouncedSync = React.useRef(
+    // Debounce sync to outer value to avoid race conditions
+    _.debounce((outerVal: number) => setTempValue(`${outerVal}`), 800, { leading: false, trailing: true })
   )
-
   React.useEffect(() => {
+    debouncedSync.current(outerValue)
+  }, [outerValue])
+
+  const updateValue = React.useCallback((valueStr: string) => {
     // Allow input of temporarily invalid data,
     // but only locally. Don't propagate the value change.
-    const parsedTempValue = parse(tempValue)
-    if (!Number.isNaN(parsedTempValue) && outerValue !== parsedTempValue) {
-      debouncedChange.current(parsedTempValue)
+    setTempValue(valueStr)
+    const parsedTempValue = parse(valueStr)
+    if (!Number.isNaN(parsedTempValue)) {
+      // Commit only valid values
+      commit(parsedTempValue)
+      // debouncedCommit.current(parsedTempValue)
     }
-  }, [tempValue, outerValue])
+  }, [])
+  const onChange = React.useCallback(
+    (e) => {
+      const parsed = parse(e.target.value)
+      // Allow temporary empty input, but prevent negative input
+      if (e.target.value === '' || parsed >= min) {
+        updateValue(`${parsed}`)
+      }
+    },
+    [updateValue, min]
+  )
+  const onBlur = React.useCallback(
+    (e) => {
+      const parsed = parse(e.target.value)
+      // Reset invalid input to previous value on blur
+      if (Number.isNaN(parsed)) {
+        updateValue(`${outerValue}`)
+      } else if (parsed > min) {
+        updateValue(`${parsed}`)
+      }
+    },
+    [updateValue, min, outerValue]
+  )
 
   return (
     <TextField
       type="number"
       value={tempValue}
-      onChange={(e) => {
-        const parsed = parse(e.target.value)
-        // Allow temporary empty input, but prevent negative input
-        if (e.target.value === '' || parsed >= min) {
-          setTempValue(`${parsed}`)
-        }
-      }}
-      onBlur={(e) => {
-        const parsed = parse(e.target.value)
-        // Reset invalid input to previous value on blur
-        if (Number.isNaN(parsed)) {
-          setTempValue(`${outerValue}`)
-        } else if (parsed > min) {
-          setTempValue(`${parsed}`)
-        }
-      }}
+      onChange={onChange}
+      onBlur={onBlur}
       onKeyDown={(e) => {
         // Handle the keyboard event locally
         e.stopPropagation()
