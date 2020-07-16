48b64dfc8f9eda932581082eae26124b09f4c5a7
Michael Adlfinger <4804412+mic-web@users.noreply.github.com>
2020-07-16 16:36:20 +0200

    Improve distinction between Work and Rest phase

--- src/components/BackgroundOverlay.tsx
+++ src/components/BackgroundOverlay.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { makeStyles, fade } from '@material-ui/core/styles'
 import { Phases } from '../types'
 import usePhase from '../selectors/usePhase'
+import { isActivePhase } from '../services/timer'
 
 const useStyles = makeStyles(({ palette }) => ({
   root: (props: { phase: Phases }) => {
@@ -9,14 +10,18 @@ const useStyles = makeStyles(({ palette }) => ({
       opacity: 0.7,
       transition: 'opacity 2s ease',
     }
+    let background = fade(palette.common.black, 0.6)
+    if (props.phase === Phases.REST) {
+      background = fade(palette.common.white, 0.6)
+    }
     return {
       position: 'absolute',
-      background: fade(palette.common.black, 0.5),
+      background,
       opacity: 0,
       transition: 'opacity 2s ease',
       width: '100%',
       height: '100%',
-      ...(props.phase === (Phases.WORK || Phases.REST) && visibleStyle),
+      ...(isActivePhase(props.phase) && visibleStyle),
     }
   },
 }))
